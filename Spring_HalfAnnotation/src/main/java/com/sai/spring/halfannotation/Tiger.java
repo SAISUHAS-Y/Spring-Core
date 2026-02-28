f66cde6db3e603d1e64ffcda732d008d60bccbab
SAISUHAS-Y <saisuhaskampli421@gmail.com>
2026-02-28 16:32:58 +0530

    Spring Half Annotation

---
+++ Spring_HalfAnnotation/src/main/java/com/sai/spring/halfannotation/Tiger.java
@@ -0,0 +1,10 @@
+package com.sai.spring.halfannotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Tiger {
+	public void sound() {
+		System.out.println("roar");
+	}
+}
