d570e5ab5ca1f8202ff55e0fb8e6306757caf7d3
SAISUHAS-Y <saisuhaskampli421@gmail.com>
2026-02-28 16:37:17 +0530

    Spring player-project

---
+++ Spring_Player_Project/Spring_Player_Project/src/main/java/com/jsp/spring/projectdb/AppConfig.java
@@ -0,0 +1,10 @@
+package com.jsp.spring.projectdb;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.jsp.spring.projectdb")
+public class AppConfig {
+
+}
